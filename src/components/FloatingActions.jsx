5a18e717db9bf213d0eb36100a7d93d61faaf1f6
RavipatiSai122 <ravipatisrisai@gmail.com>
2025-12-11 09:37:09 -0800

    Initial commit with Dockerfile

--- src/components/FloatingActions.jsx
+++ src/components/FloatingActions.jsx
@@ -41,7 +41,7 @@ export default function FloatingActions() {
     setInput("");
 
     try {
-      const response = await fetch("https://lift-chatbot.onrender.com/chat", {
+      const response = await fetch("http://127.0.0.1:8000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question }),
